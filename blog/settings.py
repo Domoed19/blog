dead0a3c999cf7df7ab1eff18741a6ba0b6fc1c2
Mikalai Simak <kilzil16@gmail.com>
2022-07-21 20:28:00 +0300

    add homework

--- blog/settings.py
+++ blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-48#q+cus&0wee)@bfbp96_q3xbilev-6p71#(^8t#0+4eqx)5q'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ.get('DATABASE_PATH'),
     }
 }
 
@@ -123,3 +123,29 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+        }
+    }
+}
+
+
+MY_PATH_ZNACH = os.environ.get('MY_PATH')
+PEREMEN_01 = (os.environ.get('PEREMEN_01'))
+PEREMEN_02 = os.environ.get('PEREMEN_02')
+PEREMEN_03 = os.environ.get('PEREMEN_03')
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,17 @@
+import logging
 from django.shortcuts import render
-
+from django.conf import settings
 from django.http import HttpResponse
 
+logger = logging.getLogger(__name__)
 
 def index(request):
+    logger.info (f"MY_PATH: {settings.MY_PATH_ZNACH}")
     if request.GET.get("key") == "test":
         return HttpResponse("Posts with test key")
+    elif request.GET.get("PEREMEN_01") == 21:
+        logger.info(f"PEREMEN_02: {settings.PEREMEN_02}")
+    else:
+        logger.info(f"PEREMEN_03: {settings.PEREMEN_03}")
     return HttpResponse("Posts index view")
+
